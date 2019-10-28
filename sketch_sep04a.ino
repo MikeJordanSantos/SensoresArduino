988b739d7466568f08e23621bd5e91233a06925d
unknown <mikejordan.tigre@gmail.com>
2019-10-28 12:17:46 -0600

    Actualizacion de codigo arduino

--- sketch_sep04a.ino
+++ sketch_sep04a.ino
@@ -1,10 +1,12 @@
 #include <DHT.h>
 #include <DHT_U.h>
 #include <Adafruit_Sensor.h>
-int pinSensor= 5;
+//float db, analog;
+float adc, dB, PdB;
+int pinSensor= 2;
 int temp,hum;
-int periodo = 5000; //1000 = 1 segundo
-unsigned long ahora = 0;
+int period = 1000; //1000 = 1 segundo
+unsigned long timeNow = 0;
 /*Sensor que usaré*/
 DHT dht (pinSensor, DHT11);
 /*Enciendo el monitor serial y el sensor*/
@@ -15,13 +17,17 @@ void setup() {
 
 void loop() {
   /*Obtengo datos y los guardo en las variables*/
+  //analog = analogRead(A0);
   temp = dht.readTemperature();
   hum =  dht.readHumidity();
-  //Serial.print("sound: ");
-  //Serial.println(analogRead(A0));
-  if(millis() > ahora + periodo){
-    ahora = millis();
-    Serial.println('{');
+  //Logaritmo para conversion a Decibeles        //db = (20*log10(analog)-.1975);  
+  PdB = dB; //Store the previous of dB here
+  adc = analogRead(A0); //Read the ADC value from amplifer
+  dB = (adc+83.2073)/ 11.003;  //Convert ADC value to dB using Regression values
+
+  if(millis() > timeNow + period){
+    timeNow = millis();
+    Serial.println('[');
     Serial.print("temperature: ");
     Serial.print(temp);
     Serial.println("°C, ");
@@ -29,13 +35,15 @@ void loop() {
     Serial.print(hum);
     Serial.println("% , ");
     Serial.print("sound: ");
-    Serial.println(analogRead(A0));
-    Serial.println('}');
+    Serial.println(dB);
+    //Serial.println(analogRead(A0));
+    Serial.println(']');
   }
-  Serial.println('{');
+  Serial.println('[');
   Serial.print("sound: ");
-  Serial.println(analogRead(A0));
-  Serial.println('}');
+  Serial.println(dB);
+  //Serial.println(analogRead(A0));
+  Serial.println(']');
 /*  File dataFile = SD.open("datalog.csv", FILE_WRITE);
   if(dataFile){
     dataFile.println("DATOS");
